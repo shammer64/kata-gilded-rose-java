57235ce6fd107bccd484b01c444f912c2d5e436b
Scott Hammer <shammer@amerisure.com>
2021-12-08 11:54:52 -0500

    SH Extracted behavior that Sulfuras never decrease Quality

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -94,4 +94,12 @@ class GildedRoseTest {
         assertEquals(0, app.items[0].sellIn);
     }
 
+    @Test
+    void sulfurasNeverDecreaseInQuality() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+    }
+
 }
