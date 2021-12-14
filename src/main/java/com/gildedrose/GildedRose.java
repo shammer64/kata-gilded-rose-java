52d1d3c6cb706bef87cf690bfd76d00e621d2272
Scott Hammer <shammer@amerisure.com>
2021-12-14 12:24:45 -0500

    SH Extracted method for Common Items; removed unused code

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -20,63 +20,18 @@ class GildedRose {
                 case "Sulfuras, Hand of Ragnaros":
                     continue;
                 default:
-                    item.quality = (item.sellIn > 0) ?
-                            (item.quality - 1) :
-                            (item.quality - 2);
-                    if (item.quality < 0) item.quality = 0;
-                    item.sellIn -= 1;
+                    handleCommonItem(item);
                     continue;
             }
-
-//            if (!items[i].name.equals("Aged Brie")
-//                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//                if (items[i].quality > 0) {
-//                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-//                        items[i].quality = items[i].quality - 1;
-//                    }
-//                }
-//            } else {
-//                if (items[i].quality < 50) {
-//                    items[i].quality = items[i].quality + 1;
-//
-//                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//                        if (items[i].sellIn < 11) {
-//                            if (items[i].quality < 50) {
-//                                items[i].quality = items[i].quality + 1;
-//                            }
-//                        }
-//
-//                        if (items[i].sellIn < 6) {
-//                            if (items[i].quality < 50) {
-//                                items[i].quality = items[i].quality + 1;
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-//                items[i].sellIn = items[i].sellIn - 1;
-//            }
-//
-//            if (items[i].sellIn < 0) {
-//                if (!items[i].name.equals("Aged Brie")) {
-//                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//                        if (items[i].quality > 0) {
-//                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-//                                items[i].quality = items[i].quality - 1;
-//                            }
-//                        }
-//                    } else {
-//                        items[i].quality = items[i].quality - items[i].quality;
-//                    }
-//                } else {
-//                    if (items[i].quality < 50) {
-//                        items[i].quality = items[i].quality + 1;
-//                    }
-//                }
-//            }
         }
+    }
+
+    private void handleCommonItem(Item item) {
+        item.quality = (item.sellIn > 0) ?
+                (item.quality - 1) :
+                (item.quality - 2);
+        if (item.quality < 0) item.quality = 0;
+        item.sellIn -= 1;
     }
 
     private void handleBackstagePass(Item item) {
