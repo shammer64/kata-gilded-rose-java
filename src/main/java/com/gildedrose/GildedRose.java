d0f40c58ec933c543e1faa3ac2962084d45f98d1
Scott Hammer <shammer@amerisure.com>
2021-12-14 14:40:51 -0500

    SH Moved factory method to ItemUpdater class

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,28 +10,8 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             ItemUpdater itemUpdater = null;
-            itemUpdater = getInstance(item);
+            itemUpdater = ItemUpdater.getInstance(item);
             itemUpdater.update();
         }
     }
-
-    private ItemUpdater getInstance(Item item) {
-        ItemUpdater itemUpdater;
-        switch (item.name) {
-            case "Aged Brie":
-                itemUpdater = new AgedBrieUpdater(item);
-                break;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                itemUpdater = new BackstagePassUpdater(item);
-                break;
-            case "Sulfuras, Hand of Ragnaros":
-                itemUpdater = new SulfurasUpdater(item);
-                break;
-            default:
-                itemUpdater = new CommonItemUpdater(item);
-                break;
-        }
-        return itemUpdater;
-    }
-
 }
--- src/main/java/com/gildedrose/ItemUpdater.java
+++ src/main/java/com/gildedrose/ItemUpdater.java
@@ -1,5 +1,24 @@
 package com.gildedrose;
 
 public abstract class ItemUpdater {
+    public static ItemUpdater getInstance(Item item) {
+        ItemUpdater itemUpdater;
+        switch (item.name) {
+            case "Aged Brie":
+                itemUpdater = new AgedBrieUpdater(item);
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                itemUpdater = new BackstagePassUpdater(item);
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                itemUpdater = new SulfurasUpdater(item);
+                break;
+            default:
+                itemUpdater = new CommonItemUpdater(item);
+                break;
+        }
+        return itemUpdater;
+    }
+
     public abstract void update();
 }
