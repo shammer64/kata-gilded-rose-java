bf9d78c390cf778aac522cf26e45a3b625505ff3
Scott Hammer <shammer@amerisure.com>
2021-12-14 13:40:29 -0500

    SH Refactored BackstagePass to update SellIn after updating quality

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -45,11 +45,11 @@ class GildedRose {
         }
 
         public void update() {
-            item.sellIn--;
-            item.quality = (item.sellIn > 9) ? item.quality + 1 :
-                    (item.sellIn >= 5 ) ? item.quality + 2 :
-                    (item.sellIn >= 0 ) ? item.quality + 3 : 0;
+            item.quality = (item.sellIn > 10) ? item.quality + 1 :
+                    (item.sellIn > 5 ) ? item.quality + 2 :
+                    (item.sellIn > 0 ) ? item.quality + 3 : 0;
             item.quality = item.quality > 50 ? 50 : item.quality;
+            item.sellIn--;
         }
     }
 
