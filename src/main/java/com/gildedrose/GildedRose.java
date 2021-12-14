ca8459139091933762ed84903a4ce6509da09101
Scott Hammer <shammer@amerisure.com>
2021-12-14 14:35:32 -0500

    SH Modified variable to be of type ItemUpdater

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,7 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            CommonItemUpdater itemUpdater = null;
+            ItemUpdater itemUpdater = null;
             switch (item.name) {
                 case "Aged Brie":
                     itemUpdater = new AgedBrieUpdater(item);
