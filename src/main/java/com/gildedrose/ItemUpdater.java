bbeba54fda44953ce28d0f6b698e8ece35ef760a
Scott Hammer <shammer@amerisure.com>
2021-12-14 15:30:50 -0500

    SH Refactored factory method to use a map of classes

--- src/main/java/com/gildedrose/ItemUpdater.java
+++ src/main/java/com/gildedrose/ItemUpdater.java
@@ -1,27 +1,40 @@
 package com.gildedrose;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class ItemUpdater {
     final Item item;
+    private static final Map<String, Class> CLASS_MAP = new HashMap<>();
+
+    static {
+        CLASS_MAP.put("Aged Brie", AgedBrieUpdater.class);
+    }
 
     public ItemUpdater(Item item) {
         this.item = item;
     }
 
     public static ItemUpdater getInstance(Item item) {
-        ItemUpdater itemUpdater;
-        switch (item.name) {
-            case "Aged Brie":
-                itemUpdater = new AgedBrieUpdater(item);
-                break;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                itemUpdater = new BackstagePassUpdater(item);
-                break;
-            case "Sulfuras, Hand of Ragnaros":
-                itemUpdater = new SulfurasUpdater(item);
-                break;
-            default:
-                itemUpdater = new CommonItemUpdater(item);
-                break;
+        ItemUpdater itemUpdater = null;
+        try {
+            switch (item.name) {
+                case "Aged Brie":
+                    itemUpdater = (ItemUpdater) CLASS_MAP.get(item.name).getDeclaredConstructor(Item.class).newInstance(item);
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    itemUpdater = new BackstagePassUpdater(item);
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    itemUpdater = new SulfurasUpdater(item);
+                    break;
+                default:
+                    itemUpdater = new CommonItemUpdater(item);
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return itemUpdater;
     }
