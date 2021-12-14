36bd3c11d9f60b43421a27ccaa3af74a3ff7ded2
Scott Hammer <shammer@amerisure.com>
2021-12-14 14:02:29 -0500

    SH Extracted updateQuality method in BackstagePass

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -45,11 +45,16 @@ class GildedRose {
         }
 
         public void update() {
+            updateQuality();
+            checkQualityBounds();
+            updateSellIn();
+        }
+
+        @Override
+        protected void updateQuality() {
             item.quality = (item.sellIn > 10) ? item.quality + 1 :
                     (item.sellIn > 5 ) ? item.quality + 2 :
                     (item.sellIn > 0 ) ? item.quality + 3 : 0;
-            checkQualityBounds();
-            updateSellIn();
         }
     }
 
