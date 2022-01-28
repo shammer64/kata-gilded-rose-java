02f2cefc542d1d310ec68d16636a78abc2ceaca2
Scott Hammer <shammer@amerisure.com>
2022-01-28 17:41:49 -0500

    SH Migrated and refactored Common Item tests

---
+++ src/test/java/com/gildedrose/CommonItemUpdaterTest.java
@@ -0,0 +1,43 @@
+package com.gildedrose;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CommonItemUpdaterTest {
+
+    @ParameterizedTest(name = "SellIn should decrease by 1")
+    @CsvSource({"1,10,0", "0,10,-1", "-1,10,-2"})
+    void shouldDecreaseSellInByOne(int sellIn, int quality, int expected) {
+        Item item = new Item("Common Item", sellIn, quality);
+        ItemUpdater itemUpdater = ItemUpdater.getInstance(item);
+
+        itemUpdater.update();
+
+        assertEquals(expected, item.sellIn);
+    }
+
+    @ParameterizedTest(name = "Quality should decrease by 1")
+    @CsvSource({"3,10,9", "2,9,8", "1,8,7"})
+    void shouldDecreaseQualityByOneBeforeSellInDate(int sellIn, int quality, int expected) {
+        Item item = new Item("Common Item", sellIn, quality);
+        ItemUpdater itemUpdater = ItemUpdater.getInstance(item);
+
+        itemUpdater.update();
+
+        assertEquals(expected, item.quality);
+    }
+
+    @ParameterizedTest(name = "Quality should never decrease below 0")
+    @CsvSource({"3,0,0", "0,0,0", "-1,1,0"})
+    void shouldNotDecreaseQualityBelowZero(int sellIn, int quality, int expected) {
+        Item item = new Item("Common Item", sellIn, quality);
+        ItemUpdater itemUpdater = ItemUpdater.getInstance(item);
+
+        itemUpdater.update();
+
+        assertEquals(expected, item.quality);
+    }
+
+}
