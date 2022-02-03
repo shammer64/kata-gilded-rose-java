4f64fa628b68d482f43a585571c90b866310ef0c
Scott Hammer <shammer@amerisure.com>
2022-02-03 17:26:07 -0500

    SH Removed commented factory method

--- src/main/java/com/gildedrose/ItemUpdaterFactory.java
+++ src/main/java/com/gildedrose/ItemUpdaterFactory.java
@@ -5,15 +5,10 @@ import java.util.Map;
 
 public class ItemUpdaterFactory {
 
-    private static final Map<String, Class> CLASS_MAP = new HashMap<>();
     private static final Map<String, ItemUpdater> INSTANCE_MAP = new HashMap<>();
     private static final ItemUpdater DEFAULT_INSTANCE = new CommonItemUpdater();
 
     static {
-        CLASS_MAP.put("Aged Brie", AgedBrieUpdater.class);
-        CLASS_MAP.put("Backstage passes to a TAFKAL80ETC concert", BackstagePassUpdater.class);
-        CLASS_MAP.put("Sulfuras, Hand of Ragnaros", SulfurasUpdater.class);
-        CLASS_MAP.put("Conjured Mana Cake", ConjuredItemUpdater.class);
         INSTANCE_MAP.put("Aged Brie", new AgedBrieUpdater());
         INSTANCE_MAP.put("Backstage passes to a TAFKAL80ETC concert", new BackstagePassUpdater());
         INSTANCE_MAP.put("Sulfuras, Hand of Ragnaros", new SulfurasUpdater());
@@ -24,15 +19,4 @@ public class ItemUpdaterFactory {
         return INSTANCE_MAP.getOrDefault(itemName, DEFAULT_INSTANCE);
     }
 
-//    @Deprecated
-//    public static ItemUpdater getInstance(Item item) {
-//        ItemUpdater itemUpdater = null;
-//        try {
-//            Class updaterClass = CLASS_MAP.getOrDefault(item.name, CommonItemUpdater.class);
-//            itemUpdater = (ItemUpdater) updaterClass.getDeclaredConstructor(Item.class).newInstance(item);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return itemUpdater;
-//    }
 }
